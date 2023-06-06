62e043e6829d9783c7aa79ec83cfcf44823d76f7
7K <kmanfinkel17@gmail.com>
2023-06-05 21:33:39 -0500

    connected project pages

--- portfolio-app/src/components/pages/projects/lead/Lead.tsx
+++ portfolio-app/src/components/pages/projects/lead/Lead.tsx
@@ -1,8 +1,26 @@
 import './Lead.scss'
 
-function Lead() {
+function Lead(props:any) {
   return (
     <>
+       <div className='lead'>{
+            props.projectList.map((project:any)=>{
+              return(<>{project.highlighted ? (<div key={project.id}>
+                    <img src={project.src} alt=''/>
+                    <h2 className='project-title'>{project.title}</h2>
+                <p className='description'>{project.desc}</p>
+                <a className='deploy' href={project.deployed} target='_blank'><button className='btn'>Project Link</button></a>
+                {project.inProgress ?  (<span>Under Development</span>):('')}
+                <a className='repo' href={project.repo} target='_blank'>
+                    <button className='btn'>Repository</button>
+                </a>
+                
+            </div> 
+            ):('')}</>)
+        })
+            
+        }
+        </div>
     </>
   )
 }
--- portfolio-app/src/components/pages/projects/list/List.tsx
+++ portfolio-app/src/components/pages/projects/list/List.tsx
@@ -1,10 +1,32 @@
-import './List.scss'
+import "./List.scss";
 
-function List() {
+function List(props: any) {
   return (
     <>
+      {props.projectList.map((project: any) => {
+        return (
+          <>
+            {!project.highlighted ? (
+              <li className="list-item" key={project.id}>
+                <img src={project.src} />
+                <h3>{project.title}</h3>
+                <p>{project.desc}</p>
+                <a className="deploy" href={project.deployed} target="_blank">
+                  <button className="btn">Project Link</button>
+                </a>
+                {project.inProgress ? <span>Under Development</span> : ""}
+                <a className="repo" href={project.repo} target="_blank">
+                  <button className="btn">Repository</button>
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+          </>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default List
+export default List;
